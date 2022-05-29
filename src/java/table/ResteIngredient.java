51d533daa2d8a9551213c3e42df155aa901a871c
dawoodNomena <dawoodnomena@gmail.com>
2022-05-29 17:22:47 +0300

    mouvement stock

---
+++ src/java/table/ResteIngredient.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package table;
+
+import connex.Connex;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Vector;
+
+/**
+ *
+ * @author Dawood
+ */
+public class ResteIngredient extends BdTable{
+    int idIngredient;
+    double reste;
+
+    public ResteIngredient constructeur(ResultSet res) throws SQLException{
+        ResteIngredient cp = new ResteIngredient(res.getInt(1),res.getDouble(2));
+        return cp;
+    }
+    
+    public ResteIngredient(int idIngredient, double reste) {
+        this.idIngredient = idIngredient;
+        this.reste = reste;
+    }
+
+    public ResteIngredient() {
+    }
+
+    public void setIdIngredient(int idIngredient) {
+        this.idIngredient = idIngredient;
+    }
+
+    public void setReste(double reste) {
+        this.reste = reste;
+    }
+
+    public int getIdIngredient() {
+        return idIngredient;
+    }
+
+    public double getReste() {
+        return reste;
+    }
+    
+    
+    public double resteIngredient(int idIngredient) throws Exception{
+        double rep = 0;
+        Connex con = new Connex();
+        String req = "select * from resteIngredient where idingredient="+idIngredient;
+        Vector liste = this.findReq(req, this, con.getCon());
+        rep = ((ResteIngredient)liste.get(0)).getReste();
+        con.deco();
+        return rep;
+    }
+    
+}
